926972385619e1b9d3a4905dd382b0aa050dc754
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2022-03-10 16:44:15 +0300

    Test: the fizzbuzz method for the solver class
    - make sure it accepts just one argument
    - make sure it accepts just integers as a argument
    - make sure it returns the result as a string
    - make sure it returns "fizz" when param is divisible by 3
    - make sure it returns "buzz" when param is divisible by 5
    - make sure returns "fizzbuzz" when param is divisible by 3 and 5
    -make sure it returns "7" when  param is 7

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -38,4 +38,40 @@ describe 'Solver' do
       expect{ @solver.reverse('word','secondstr') }.to raise_exception(ArgumentError)
     end
   end
+
+  context '#fizzbuzz' do
+    it 'should accept just one argument' do
+      expect{ @solver.fizzbuzz(6,10) }.to raise_exception(ArgumentError)
+    end
+
+    it 'should accept just integers as a argument' do
+      expect{ @solver.fizzbuzz("aa") }.to raise_exception(TypeError)
+    end
+
+    it 'should return the result as a string' do
+      expect{ @solver.fizzbuzz(5) }.to be_instance_of(String)
+    end
+
+    it 'should return "fizz" when param is divisible by 3 ' do
+      expect{ @solver.fizzbuzz(3) }.to eq 'fizz' 
+      expect{ @solver.fizzbuzz(6) }.to eq 'fizz' 
+      expect{ @solver.fizzbuzz(9) }.to eq 'fizz' 
+    end
+
+    it 'should return "buzz" when param is divisible by 5 ' do
+      expect{ @solver.fizzbuzz(5) }.to eq 'buzz' 
+      expect{ @solver.fizzbuzz(10) }.to eq 'buzz' 
+      expect{ @solver.fizzbuzz(20) }.to eq 'buzz' 
+    end
+
+    it 'should return "fizzbuzz" when param is divisible by 3 and 5' do
+      expect{ @solver.fizzbuzz(15) }.to eq 'fizzbuzz' 
+      expect{ @solver.fizzbuzz(30) }.to eq 'fizzbuzz' 
+      expect{ @solver.fizzbuzz(900) }.to eq 'fizzbuzz' 
+    end
+
+    it 'should return "7" when  param is 7' do
+      expect{ @solver.fizzbuzz(7) }.to eq '7' 
+    end
+  end
 end
