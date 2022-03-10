e15e3f1a310725487d1b83444d4150c46a69cc3d
Anselem <anselemodims@gmail.com>
2022-03-10 15:12:31 +0100

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,10 +1,8 @@
 class Solver
-
   def factorial(num)
-    if num < 0
-      raise TypeError.new("number should not be a negative integer")
-    end
-    num == 0 ? 1 : num * factorial(num-1)
+    raise TypeError, 'number should not be a negative integer' if num.negative?
+
+    num.zero? ? 1 : num * factorial(num - 1)
   end
 
   def reverse(word)
@@ -12,15 +10,16 @@ class Solver
   end
 
   def fizzbuzz(num)
-    raise TypeError.new('value mmust be a number') if !num.is_a?(Integer) 
-    if num % 3 == 0 && num % 5 == 0 
+    raise TypeError, 'value mmust be a number' unless num.is_a?(Integer)
+
+    if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
-    elsif num % 3 == 0
-      'fizz' 
-    elsif num % 5 == 0 
-      'buzz' 
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
     else
-     "#{num}"
+      num.to_s
     end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -19,11 +19,11 @@ describe 'Solver' do
 
     it 'should return error if number is not a positive integer' do
       num = -1
-      expect{ @solver.factorial(num) }.to raise_exception(TypeError)
+      expect { @solver.factorial(num) }.to raise_exception(TypeError)
     end
   end
 
-  context '#reverse' do 
+  context '#reverse' do
     it 'word string should returned as drow' do
       reversed_word = @solver.reverse('word')
       expect(reversed_word).to eq 'drow'
@@ -35,17 +35,17 @@ describe 'Solver' do
     end
 
     it 'function should be given just one paramater' do
-      expect{ @solver.reverse('word','secondstr') }.to raise_exception(ArgumentError)
+      expect { @solver.reverse('word', 'secondstr') }.to raise_exception(ArgumentError)
     end
   end
 
   context '#fizzbuzz' do
     it 'should accept just one argument' do
-      expect{ @solver.fizzbuzz(6,10) }.to raise_exception(ArgumentError)
+      expect { @solver.fizzbuzz(6, 10) }.to raise_exception(ArgumentError)
     end
 
     it 'should accept just integers as a argument' do
-      expect{ @solver.fizzbuzz("aa") }.to raise_exception(TypeError)
+      expect { @solver.fizzbuzz('aa') }.to raise_exception(TypeError)
     end
 
     it 'should return the result as a string' do
@@ -53,25 +53,25 @@ describe 'Solver' do
     end
 
     it 'should return "fizz" when param is divisible by 3 ' do
-      expect(@solver.fizzbuzz(3)).to eq 'fizz' 
-      expect(@solver.fizzbuzz(6) ).to eq 'fizz' 
-      expect(@solver.fizzbuzz(9)).to eq 'fizz' 
+      expect(@solver.fizzbuzz(3)).to eq 'fizz'
+      expect(@solver.fizzbuzz(6)).to eq 'fizz'
+      expect(@solver.fizzbuzz(9)).to eq 'fizz'
     end
 
     it 'should return "buzz" when param is divisible by 5 ' do
-      expect(@solver.fizzbuzz(5)).to eq 'buzz' 
-      expect(@solver.fizzbuzz(10)).to eq 'buzz' 
-      expect(@solver.fizzbuzz(20)).to eq 'buzz' 
+      expect(@solver.fizzbuzz(5)).to eq 'buzz'
+      expect(@solver.fizzbuzz(10)).to eq 'buzz'
+      expect(@solver.fizzbuzz(20)).to eq 'buzz'
     end
 
     it 'should return "fizzbuzz" when param is divisible by 3 and 5' do
-      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz' 
-      expect(@solver.fizzbuzz(30)).to eq 'fizzbuzz' 
-      expect(@solver.fizzbuzz(900)).to eq 'fizzbuzz' 
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+      expect(@solver.fizzbuzz(30)).to eq 'fizzbuzz'
+      expect(@solver.fizzbuzz(900)).to eq 'fizzbuzz'
     end
 
     it 'should return "7" when  param is 7' do
-      expect(@solver.fizzbuzz(7)).to eq '7' 
+      expect(@solver.fizzbuzz(7)).to eq '7'
     end
   end
 end
